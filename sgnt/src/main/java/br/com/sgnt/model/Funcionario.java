0be154aa8c101bc40de5331aa5dee69acba2d439
caiovpalmeidadjr <34443195+caiovpalmeidadjr@users.noreply.github.com>
2019-02-28 12:53:39 -0300

    implementação do relacionamento com usuario

--- sgnt/src/main/java/br/com/sgnt/model/Funcionario.java
+++ sgnt/src/main/java/br/com/sgnt/model/Funcionario.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -51,6 +52,10 @@ public class Funcionario {
 
 	@Column(name = "ATIVO")
 	private boolean ativo;
+	
+	@OneToOne
+	@Column(name="ID_USUA") 
+	private Usuario usuario;
 
 	public Integer getMatriculaFunc() {
 		return matriculaFunc;
