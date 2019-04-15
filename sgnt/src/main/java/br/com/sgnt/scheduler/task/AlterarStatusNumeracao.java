bbf65107488d94595a52feb3370531a46b842a4a
caiovpalmeidadjr <34443195+caiovpalmeidadjr@users.noreply.github.com>
2019-04-15 12:54:39 -0300

    Gravando log

--- sgnt/src/main/java/br/com/sgnt/scheduler/task/AlterarStatusNumeracao.java
+++ sgnt/src/main/java/br/com/sgnt/scheduler/task/AlterarStatusNumeracao.java
@@ -3,8 +3,11 @@ package br.com.sgnt.scheduler.task;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.Properties;
 
 import javax.faces.context.FacesContext;
@@ -33,14 +36,24 @@ public class AlterarStatusNumeracao {
 	 * F: Dia da semana (0 – 6).
 	 * (* * * ignora os campos, qlq valor naqueles campos)*/
 
-	@Scheduled(cron = "0 53 17 * * *", zone = TIME_ZONE)
-	public void verificarStausNumeracaoSTFC() {
+	@Scheduled(cron = "0 43 15 * * *", zone = TIME_ZONE)
+	public void verificarStausNumeracaoSTFC() throws IOException{
 		
 		Properties props;
 		
 		String pathSTFC = "";
+		String pathLogSTFC = "";
+		
+		FileWriter arquivoLog = null;
 		
 		FileInputStream file;
+		
+		Calendar cal = Calendar.getInstance();
+		
+	    SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyHHmmss");
+	    
+	    String strDate = sdf.format(cal.getTime());
+	    
 		try {
 			
 			
@@ -51,28 +64,47 @@ public class AlterarStatusNumeracao {
 			
 			pathSTFC = props.getProperty("prop.path.stfc");
 			
+			pathLogSTFC = props.getProperty("prop.path.log.stfc");
+			
+			arquivoLog = new FileWriter(new File(pathLogSTFC+"\\logSTFC"+strDate+".txt"));
+			
 			File arquivoRaiz = new File(pathSTFC);
 			
-			for(File f : arquivoRaiz.listFiles()) {
+			arquivoLog.write("----- Atualização de Status STFC -----" + "\r\n");
+			
+			for(File fileEncontrado : arquivoRaiz.listFiles()) {
+				
 				//1 - encontrar arquivo com inicial stfc
-				//2 - ler arquivo 
+				if(fileEncontrado.getName().contains("stfc")) {
+					//escrevendo no arquivo de log
+					arquivoLog.write("INFO: Encontramos o arquivo:" + fileEncontrado.getName() + "\r\n");
+					System.out.println(fileEncontrado.getName() +"simmmm");
+					//2 - ler arquivo 
+				}else {
+					arquivoLog.write("INFO: Arquivo não processado:" + fileEncontrado.getName() + "\r\n");
+					System.out.println(fileEncontrado.getName() +"nãooooo");
+				}
+				
+				
 				//3 - ignorar cabeçalho
 				//4 - para cada linha, verificar no banco o valor do status do numero com o status da linha passado e ai sim mudar caso seja possivel
 				//5 - caso atenda a situação eu atualizo
 				//6 - caso contrario não faço nada, apenas escrevo no log
 				//7 - ler a proxima linha do arquivo
 				//8 - Fim do processo
-				System.out.println(f.getName());
+				//System.out.println(file.getName());
 			}
 			
 			
-			
+			arquivoLog.close();
 			System.out.println(props.getProperty("prop.path.stfc"));
 			
 		} catch (FileNotFoundException e) {
+			arquivoLog.write("ERROR: Foi encontrado um erro:" + e.getMessage() + "\r\n");
 			e.printStackTrace();
 			
 		} catch (IOException e) {
+			arquivoLog.write("ERROR: Foi encontrado um erro:" + e.getMessage() + "\r\n");
 			e.printStackTrace();
 			
 		}
