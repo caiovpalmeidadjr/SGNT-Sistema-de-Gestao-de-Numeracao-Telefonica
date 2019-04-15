ef2cb093d925af5e1a83dd76abdfecf27125a732
caiovpalmeidadjr <34443195+caiovpalmeidadjr@users.noreply.github.com>
2019-04-15 17:28:40 -0300

    Desenvolvimento alterarStatus Rotina

--- sgnt/src/main/java/br/com/sgnt/scheduler/task/AlterarStatusNumeracao.java
+++ sgnt/src/main/java/br/com/sgnt/scheduler/task/AlterarStatusNumeracao.java
@@ -1,14 +1,17 @@
 package br.com.sgnt.scheduler.task;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Properties;
+import java.util.StringTokenizer;
 
 import javax.faces.context.FacesContext;
 
@@ -56,6 +59,17 @@ public class AlterarStatusNumeracao {
 	    
 		try {
 			
+			FileReader reader;
+			
+			BufferedReader leitor;
+	         
+	        StringTokenizer st = null;
+	        
+	        String cn;        
+	        String prefixo;   
+	        String mcdu;
+	        String status;
+	        String linha;
 			
 			file = new FileInputStream("C:\\config\\config.properties");
 			
@@ -76,10 +90,35 @@ public class AlterarStatusNumeracao {
 				
 				//1 - encontrar arquivo com inicial stfc
 				if(fileEncontrado.getName().contains("stfc")) {
+					
 					//escrevendo no arquivo de log
 					arquivoLog.write("INFO: Encontramos o arquivo:" + fileEncontrado.getName() + "\r\n");
-					System.out.println(fileEncontrado.getName() +"simmmm");
+					
+					System.out.println(arquivoRaiz+"\\"+fileEncontrado.getName() +"simmmm");
+					
 					//2 - ler arquivo 
+					reader = new FileReader(arquivoRaiz+"\\"+fileEncontrado.getName()); // Localização do Arquivo
+					
+					leitor = new BufferedReader(reader);
+					
+					while((linha = leitor.readLine()) !=null) {
+					
+						//delimitador parar dividir os campos
+						st = new StringTokenizer(linha,  ";");
+						
+						String dados = "";
+						
+						while(st.hasMoreTokens()) {
+							
+							//pegandos campos da linha e colocando em variaveis
+							cn = st.nextToken();
+							prefixo = st.nextToken();
+							mcdu = st.nextToken();
+							status = st.nextToken();
+						}
+					
+					}
+					
 				}else {
 					arquivoLog.write("INFO: Arquivo não processado:" + fileEncontrado.getName() + "\r\n");
 					System.out.println(fileEncontrado.getName() +"nãooooo");
