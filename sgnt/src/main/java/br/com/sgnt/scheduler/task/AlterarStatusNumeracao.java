6e302e0349b15cb20611c64098db1fb9d188d0b4
Andre Coutinho <andrecapie@hotmail.com>
2019-05-01 14:31:43 -0300

    Alterar Numeracao CNG

--- sgnt/src/main/java/br/com/sgnt/scheduler/task/AlterarStatusNumeracao.java
+++ sgnt/src/main/java/br/com/sgnt/scheduler/task/AlterarStatusNumeracao.java
@@ -19,9 +19,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import br.com.sgnt.model.NumeroCNG;
 import br.com.sgnt.model.NumeroSTFC;
 import br.com.sgnt.model.Status;
 import br.com.sgnt.service.IAreaLocalService;
+import br.com.sgnt.service.INumeroCNGService;
 import br.com.sgnt.service.INumeroSTFCService;
 
 @Component
@@ -40,6 +42,9 @@ public class AlterarStatusNumeracao {
 	@Autowired
 	private IAreaLocalService areaLocalService;
 	
+	@Autowired
+	private INumeroCNGService cngService;
+	
 	/*
 	 * A B C D E F
 	 * A: Segundos (0 – 59).
@@ -50,8 +55,6 @@ public class AlterarStatusNumeracao {
 	 * F: Dia da semana (0 – 6).
 	 * (* * * ignora os campos, qlq valor naqueles campos)*/
 	
-	@Scheduled(cron = "* 16 15 * * *", zone = TIME_ZONE)
-	//@Scheduled(cron = "* 0/1 * * * *", zone = TIME_ZONE)
 	public void verificarStausNumeracaoSTFC() throws IOException{
 		
 		Properties props;
@@ -203,5 +206,166 @@ public class AlterarStatusNumeracao {
 		}
 		
 	}
+	
+	
+	
+	
+	public void verificarStausNumeracaoCNG() throws IOException{
+		
+		Properties props;
+		
+		String pathCNG = "";
+		String pathLogCNG = "";
+		
+		FileWriter arquivoLog = null;
+		
+		FileInputStream file;
+		
+		Calendar cal = Calendar.getInstance();
+		
+	    SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyHHmmss");
+	    
+	    String strDate = sdf.format(cal.getTime());
 
+	    
+		try {
+			
+			FileReader reader;
+			
+			BufferedReader leitor;
+	         
+	        StringTokenizer st = null;
+	        
+	        String prefixo;        
+	        String serie;   
+	        String mcdu;
+	        String status;
+	        String linha;
+			
+			file = new FileInputStream("C:\\config\\config.properties");
+			
+			props = new Properties();
+			props.load(file);
+			
+			pathCNG = props.getProperty("prop.path.cng");
+			
+			pathLogCNG = props.getProperty("prop.path.log.cng");
+			
+			arquivoLog = new FileWriter(new File(pathLogCNG+"\\logCNG"+strDate+".txt"));
+			
+			File arquivoRaiz = new File(pathCNG);
+			
+			arquivoLog.write("----- Atualização de Status CMG -----" + "\r\n");
+			
+			for(File fileEncontrado : arquivoRaiz.listFiles()) {
+				
+				//encontrar arquivo com inicial stfc
+				if(fileEncontrado.getName().contains("cng")) {
+					
+					//escrevendo no arquivo de log
+					arquivoLog.write("INFO: Encontramos o arquivo:" + fileEncontrado.getName() + "\r\n");
+					
+					System.out.println(arquivoRaiz+"\\"+fileEncontrado.getName() +"simmmm");
+					
+					//ler arquivo 
+					reader = new FileReader(arquivoRaiz+"\\"+fileEncontrado.getName()); // Localização do Arquivo
+					
+					leitor = new BufferedReader(reader);
+					
+					while((linha = leitor.readLine()) !=null) {
+					
+						//delimitador parar dividir os campos
+						st = new StringTokenizer(linha,  ";");
+						
+						while(st.hasMoreTokens()) {
+							
+							//pegandos campos da linha e colocando em variaveis
+							prefixo = st.nextToken();
+							serie = st.nextToken();
+							mcdu = st.nextToken();
+							status = st.nextToken();
+							
+							int statusConvertido = Integer.parseInt(status);
+							
+							//para cada linha, verificar no banco o valor do status do numero com o status da linha passado e ai sim mudar caso seja possivel
+							NumeroCNG numCNGBuscado = cngService.findNumero(Integer.parseInt(prefixo), Integer.parseInt(serie), Integer.parseInt(mcdu));
+							
+							if(numCNGBuscado != null) {
+								//Se status arquivo = "ATIVADO" E status banco = "RESERVADO"
+								//Altera Status no banco para "ATIVADO
+								if(statusConvertido == 3 && numCNGBuscado.getStatus().getIdStatus() == 2) {
+									Status novoStatus = new Status();
+									novoStatus.setIdStatus(3);
+									
+									numCNGBuscado.setStatus(novoStatus);
+									numCNGBuscado.setDataHoraStatus(new Timestamp(System.currentTimeMillis()));
+									
+									cngService.atualizar(numCNGBuscado);
+									arquivoLog.write("INFO: Atualizando Status para 3(ATIVADO) do STFC com ID: " + numCNGBuscado.getIdNumeroCNG() + "\r\n");
+								
+								//Se status arquivo = "DESATIVADO" E status banco = "ATIVADO"
+							    //Altera status no banco para "DESATIVADO"	
+								}else if (statusConvertido == 4 && numCNGBuscado.getStatus().getIdStatus() == 3) {
+									Status novoStatus = new Status();
+									novoStatus.setIdStatus(4);
+									
+									numCNGBuscado.setStatus(novoStatus);
+									numCNGBuscado.setDataHoraStatus(new Timestamp(System.currentTimeMillis()));
+									
+									cngService.atualizar(numCNGBuscado);
+									arquivoLog.write("INFO: Atualizando Status para 4(DESATIVADO) do STFC com ID: " + numCNGBuscado.getIdNumeroCNG() + "\r\n");
+									
+								}else {
+									//Senão
+									//Não altera o status no banco e somente registra em log o numero, status do txt e status do banco
+									arquivoLog.write("INFO: Status invalido para ser modificado" + "\r\n");
+								
+								}
+									
+							}
+							
+							//System.out.println("cn"+cn+" prefixo"+prefixo+" mcdu"+mcdu+" status"+status);
+							
+							//stfcService.atualizar(reader);
+						}
+					
+					}
+					
+				}else {
+					arquivoLog.write("INFO: Arquivo não processado:" + fileEncontrado.getName() + "\r\n");
+					System.out.println(fileEncontrado.getName() +"nãooooo");
+				}
+				
+				
+				//3 - ignorar cabeçalho
+				
+				
+				//6 - caso contrario não faço nada, apenas escrevo no log
+				//7 - ler a proxima linha do arquivo
+				//8 - Fim do processo
+				//System.out.println(file.getName());
+			}
+			
+			arquivoLog.write("INFO: Fim do Processo de Atualização" + "\r\n");
+			arquivoLog.close();
+			System.out.println(props.getProperty("prop.path.cng"));
+			
+		} catch (FileNotFoundException e) {
+			arquivoLog.write("ERROR: Foi encontrado um erro:" + e.getMessage() + "\r\n");
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			arquivoLog.write("ERROR: Foi encontrado um erro:" + e.getMessage() + "\r\n");
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+	@Scheduled(cron = "* 16 15 * * *", zone = TIME_ZONE)
+	public void verificarStausNumeracao() throws IOException {
+		verificarStausNumeracaoSTFC();
+		verificarStausNumeracaoCNG();
+	}
+	
 }
